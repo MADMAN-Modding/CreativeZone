3318b22c0c507c10e25fa00212b1da2f1ef8a358
MADMAN-Modding <mad@kmdtech.net>
2026-01-02 15:24:03 -0500

    success in creating a command for making zones

--- CreativeZone/src/main/java/dev/madtechs/creativeZone/CreativeZone.java
+++ CreativeZone/src/main/java/dev/madtechs/creativeZone/CreativeZone.java
@@ -1,44 +1,26 @@
 package dev.madtechs.creativeZone;
 
-import dev.madtechs.creativeZone.voidWorld.VoidWorld;
-import dev.madtechs.creativeZone.voidWorld.ZoneMaker;
-import net.kyori.adventure.text.Component;
+import dev.madtechs.creativeZone.commands.ZoneCommand;
 import org.bukkit.*;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CreativeZone extends JavaPlugin implements Listener {
+    private static CreativeZone instance;
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+
+        instance = this;
+
+        PluginCommand zoneCommand = getCommand("makeZone");
+
+        zoneCommand.setExecutor(new ZoneCommand());
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName()));
-
-        Player player = event.getPlayer();
-
-        WorldCreator creativeZone = VoidWorld.getVoidWorld();
-
-        World currentWorld = new WorldCreator("world").createWorld();
-
-        // Generate the creative world
-        World creativeWorld = creativeZone.createWorld();
-
-        player.setGameMode(GameMode.CREATIVE);
-
-        Location playerLocation = player.getLocation();
-
-        playerLocation.setWorld(creativeWorld);
-
-        player.teleport(playerLocation);
-
-        // Set the data
-        ZoneMaker.makeWorld(creativeWorld, currentWorld, 4, player.getLocation(), this);
-
+    public static CreativeZone getInstance() {
+        return instance;
     }
 }
